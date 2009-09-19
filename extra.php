224b5134b8252899323dcc48ea89a5b11ab7c076
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-19 09:36:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/vertical-scroll-recent-post/trunk@156180 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ extra.php
@@ -0,0 +1,21 @@
+<?php
+function timepass() 
+{
+	?>
+	<div align="center">
+	<script type="text/javascript"><!--
+	google_ad_client = "pub-6676019656533652";
+	/* 200x200, created 8/27/09 */
+	google_ad_slot = "8830221633";
+	google_ad_width = 200;
+	google_ad_height = 200;
+	//-->
+	</script>
+	<script type="text/javascript"
+	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>
+	</div>
+    <?php
+	return timepass;
+}
+?>
